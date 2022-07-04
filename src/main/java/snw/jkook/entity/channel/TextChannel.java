427a6e1819c26637107c5a3351c3d17d567e94f3
ZX夏夜之风 <snwcreations@qq.com>
2022-07-04 10:07:48 +0800

    Add new argument to TextChannel#sendComponent.

--- src/main/java/snw/jkook/entity/channel/TextChannel.java
+++ src/main/java/snw/jkook/entity/channel/TextChannel.java
@@ -1,5 +1,8 @@
 package snw.jkook.entity.channel;
 
+import org.jetbrains.annotations.Nullable;
+import snw.jkook.entity.User;
+import snw.jkook.message.Message;
 import snw.jkook.message.component.BaseComponent;
 
 /**
@@ -10,7 +13,11 @@ public interface TextChannel extends Channel {
     /**
      * Send a message to this channel.
      *
-     * @param component The message to send
+     * @param component  The message to send
+     * @param quote      If this parameter is passed in, the incoming message
+     *                   will be considered a reply to the message corresponding to this parameter
+     * @param tempTarget If you pass this parameter,
+     *                   only the user to whom it corresponds can see the incoming message
      */
-    void sendComponent(BaseComponent component);
+    void sendComponent(BaseComponent component, @Nullable Message quote, @Nullable User tempTarget);
 }
