d476dcbd908a4d615a314e1df88e5b569b30d411
ZX夏夜之风 <snwcreations@qq.com>
2022-07-04 15:09:10 +0800

    Add some methods to Channel.

--- src/main/java/snw/jkook/entity/channel/Channel.java
+++ src/main/java/snw/jkook/entity/channel/Channel.java
@@ -1,5 +1,7 @@
 package snw.jkook.entity.channel;
 
+import snw.jkook.entity.Role;
+import snw.jkook.entity.User;
 import snw.jkook.entity.abilities.Nameable;
 
 /**
@@ -18,7 +20,78 @@ public interface Channel extends Nameable {
     boolean isPermissionSync();
 
     /**
-     * Get the group that holds this channel.
+     * Get the limit of minimum speaking time between two statements. (in seconds)
      */
-    ChannelGroup getParent();
+    int getChatLimitTime();
+
+    /**
+     * Return true if this channel is {@link Category}.
+     */
+    default boolean isCategory() {
+        return (this instanceof Category);
+    }
+
+    /**
+     * Get the category that holds this channel.
+     */
+    Category getParent();
+
+    /**
+     * Return true if this channel has been protected using password.
+     */
+    boolean hasPassword();
+
+    /**
+     * Represents the overwritten permissions for a role in the channel.
+     */
+    class RolePermissionOverwrite {
+        private final Role role;
+        private final int rawAllow;
+        private final int rawDeny;
+
+        public RolePermissionOverwrite(Role role, int rawAllow, int rawDeny) {
+            this.role = role;
+            this.rawAllow = rawAllow;
+            this.rawDeny = rawDeny;
+        }
+
+        public Role getRole() {
+            return role;
+        }
+
+        public int getRawAllow() {
+            return rawAllow;
+        }
+
+        public int getRawDeny() {
+            return rawDeny;
+        }
+    }
+
+    /**
+     * Represents the overwritten permissions for a user in the channel.
+     */
+    class UserPermissionOverwrite {
+        private final User user;
+        private final int rawAllow;
+        private final int rawDeny;
+
+        public UserPermissionOverwrite(User user, int rawAllow, int rawDeny) {
+            this.user = user;
+            this.rawAllow = rawAllow;
+            this.rawDeny = rawDeny;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public int getRawAllow() {
+            return rawAllow;
+        }
+
+        public int getRawDeny() {
+            return rawDeny;
+        }
+    }
 }
