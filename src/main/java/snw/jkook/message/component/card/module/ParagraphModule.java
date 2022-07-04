b58297d21c1d9f5dab06326e1d33f589b545b23b
ZX夏夜之风 <snwcreations@qq.com>
2022-07-04 10:06:44 +0800

    The content of ParagraphModule shouldn't be modified.

--- src/main/java/snw/jkook/message/component/card/module/ParagraphModule.java
+++ src/main/java/snw/jkook/message/component/card/module/ParagraphModule.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.Validate;
 import snw.jkook.entity.abilities.Accessory;
 import snw.jkook.entity.abilities.AccessoryHolder;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,36 +12,21 @@ import java.util.List;
  * Represents the module with columns.
  */
 public class ParagraphModule extends BaseModule implements AccessoryHolder {
-    private final List<PlainTextModule> modules = new LinkedList<>();
+    private final List<PlainTextModule> modules;
     private final int cols;
     private final Accessory accessory;
 
-    public ParagraphModule(int cols, Accessory accessory) {
+    public ParagraphModule(int cols, List<PlainTextModule> modules, Accessory accessory) {
+        this.modules = Collections.unmodifiableList(new LinkedList<>(modules));
         Validate.isTrue(cols > 0, "Columns cannot be negative.");
         Validate.isTrue(cols <= 3, "Columns cannot be greater than 3."); // Kook Official limit!
         this.cols = cols;
         this.accessory = accessory;
     }
 
-    public int size() {
-        return modules.size();
-    }
-
-    public boolean add(PlainTextModule plainTextModule) {
-        if (modules.size() > 50) {
-            throw new IllegalStateException("The paragraph modules can hold up to 50 elements.");
-        }
-        return modules.add(plainTextModule);
-    }
-
-    public boolean remove(PlainTextModule o) {
-        return modules.remove(o);
-    }
-
-    public void clear() {
-        modules.clear();
-    }
-
+    /**
+     * Get the columns of this module.
+     */
     public int getColumns() {
         return cols;
     }
@@ -48,5 +34,12 @@ public class ParagraphModule extends BaseModule implements AccessoryHolder {
     @Override
     public Accessory getAccessory() {
         return accessory;
+    }
+
+    /**
+     * Get the modules stored by this module.
+     */
+    public List<PlainTextModule> getModules() {
+        return modules;
     }
 }
