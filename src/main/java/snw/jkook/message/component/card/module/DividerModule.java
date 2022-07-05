75e4b4019ae3d374a5ad6afde02b9e82f4dc6ca4
ZX夏夜之风 <snwcreations@qq.com>
2022-07-05 10:06:46 +0800

    The constructor of DividerModule is protected now.

--- src/main/java/snw/jkook/message/component/card/module/DividerModule.java
+++ src/main/java/snw/jkook/message/component/card/module/DividerModule.java
@@ -4,4 +4,13 @@ package snw.jkook.message.component.card.module;
  * Represents a divider module.
  */
 public class DividerModule extends BaseModule {
+
+    /** Only one the instance of this module. One per VM. */
+    public static final DividerModule INSTANCE = new DividerModule();
+
+    /*
+       Because this class does not provide more operations,
+       so I think it is not necessary to make the constructor public.
+    */
+    protected DividerModule() {}
 }
