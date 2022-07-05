f92d6b0f2f9ef9ec619be98e6fd0e20ec12938d2
ZX夏夜之风 <snwcreations@qq.com>
2022-07-05 13:52:49 +0800

    Remove "@throws IllegalArgumentException" for BotLoader#loadBot.

--- src/main/java/snw/jkook/bot/BotLoader.java
+++ src/main/java/snw/jkook/bot/BotLoader.java
@@ -12,8 +12,7 @@ public interface BotLoader {
      *
      * @param file The Bot data source
      * @return The Bot instance
-     * @throws IllegalArgumentException Thrown if the Bot file is invalid or token is empty
-     * @throws InvalidBotException      Thrown if an error occurred while the loader attempting to load the Bot
+     * @throws InvalidBotException Thrown if an error occurred while the loader attempting to load the Bot
      */
     Bot loadBot(final File file, final String token) throws InvalidBotException;
 }
