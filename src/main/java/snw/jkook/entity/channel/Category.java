eb41b2fe225cc8a122d3182eb61e1a8766683023
ZX夏夜之风 <snwcreations@qq.com>
2022-07-07 10:34:20 +0800

    Fix possible critical problem at Category.

--- src/main/java/snw/jkook/entity/channel/Category.java
+++ src/main/java/snw/jkook/entity/channel/Category.java
@@ -1,5 +1,6 @@
 package snw.jkook.entity.channel;
 
+import org.jetbrains.annotations.Contract;
 import snw.jkook.entity.abilities.MasterHolder;
 import snw.jkook.entity.abilities.Nameable;
 
@@ -9,6 +10,18 @@ import java.util.Collection;
  * Represents a group of channel.
  */
 public interface Category extends Channel, Nameable, MasterHolder {
+
+    @Contract("_, _-> fail")
+    @Override
+    default String createInvite(int validSeconds, int validTimes) {
+        throw new UnsupportedOperationException("Kook does not support the users inviting other users to a category.");
+    }
+
+    @Contract("_ -> fail")
+    @Override
+    default void removeInvite(String urlCode) {
+        throw new UnsupportedOperationException("Kook does not support the users inviting other users to a category.");
+    }
 
     /**
      * Return the channels in this group.
