f95701da6efce513c9ef640a407048432c4aa35d
ZX夏夜之风 <snwcreations@qq.com>
2022-07-08 16:08:13 +0800

    Deprecate CustomEmoji#getUrl.

--- src/main/java/snw/jkook/entity/CustomEmoji.java
+++ src/main/java/snw/jkook/entity/CustomEmoji.java
@@ -1,5 +1,6 @@
 package snw.jkook.entity;
 
+import org.jetbrains.annotations.Contract;
 import snw.jkook.Permission;
 import snw.jkook.util.RequirePermission;
 
@@ -10,7 +11,12 @@ public interface CustomEmoji {
 
     /**
      * Get the image url of this emoji.
+     *
+     * @return Always return <code>null</code>
+     * @deprecated This method can't work because the Kook official does <b>NOT</b> provide URL.
      */
+    @Contract("-> null")
+    @Deprecated
     String getUrl();
 
     /**
