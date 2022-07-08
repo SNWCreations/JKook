b46eb87dadc4bfe4b2f35eaff5e5a3417fca9640
ZX夏夜之风 <snwcreations@qq.com>
2022-07-08 16:19:29 +0800

    Update the document at AvatarHolder#getAvatarUrl.

--- src/main/java/snw/jkook/entity/abilities/AvatarHolder.java
+++ src/main/java/snw/jkook/entity/abilities/AvatarHolder.java
@@ -9,6 +9,7 @@ public interface AvatarHolder {
      * Get the avatar url of this object.
      *
      * @param vip True if you want the url of the vip avatar.
+     * @return The avatar url. Sometimes, whether true or false is provided, the result may be the same, because the object may not be a VIP.
      */
     String getAvatarUrl(boolean vip);
 }
