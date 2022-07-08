ba6ef87e24df5ed83828df5439c80e197ed172b8
ZX夏夜之风 <snwcreations@qq.com>
2022-07-08 15:25:28 +0800

    Add "pageSize" attribute to PageIterator.

--- src/main/java/snw/jkook/util/PageIterator.java
+++ src/main/java/snw/jkook/util/PageIterator.java
@@ -3,7 +3,8 @@ package snw.jkook.util;
 import java.util.Iterator;
 
 /**
- * Represents the iterator of something can have pages.
+ * Represents the iterator of something can have pages. <p>
+ * Default page size is <code>50</code>, you can change it by using {@link #setPageSize(int)}.
  *
  * @param <T> The type of thing will be provided
  */
@@ -19,4 +20,15 @@ public interface PageIterator<T> extends Iterator<T> {
     default boolean hasNext() {
         throw new UnsupportedOperationException("hasNext");
     }
+
+    /**
+     * Get current size of the pages that iterator provides.
+     */
+    int getPageSize();
+
+    /**
+     * Set the size of the pages that this iterator provides.
+     * @param size The size to set
+     */
+    void setPageSize(int size);
 }
