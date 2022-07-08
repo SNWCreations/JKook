2c3437da98fa53ef40935aa270be456f44069c77
ZX夏夜之风 <snwcreations@qq.com>
2022-07-08 15:01:44 +0800

    Add TextChannel#getMessages.

--- src/main/java/snw/jkook/entity/channel/TextChannel.java
+++ src/main/java/snw/jkook/entity/channel/TextChannel.java
@@ -1,14 +1,33 @@
 package snw.jkook.entity.channel;
 
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
 import snw.jkook.entity.User;
 import snw.jkook.message.Message;
+import snw.jkook.message.TextChannelMessage;
 import snw.jkook.message.component.BaseComponent;
+
+import java.util.Collection;
 
 /**
  * Represents a channel that can chat using texts.
  */
 public interface TextChannel extends Channel {
+
+    /**
+     * Get the messages in this channel by given information.
+     *
+     * @param refer The reference message, remote will query the data around this. <p> Null to get the latest message
+     * @param isPin True if query pinned message. If you provide true, then you must provide null to "refer" argument, and only the pinned messages will be returned
+     * @param queryMode The query mode. Only accepts "before", "around" and "after". Case Sensitive!
+     * @param pageSize The size of the page
+     */
+    Collection<TextChannelMessage> getMessages(
+            @Nullable String refer,
+            boolean isPin,
+            String queryMode,
+            @Range(from = 1, to = 100) int pageSize
+    );
 
     /**
      * Send a message to this channel.
