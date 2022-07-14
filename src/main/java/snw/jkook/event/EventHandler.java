aefa5d7b89cb933156204d9965631e5e0dd2aa73
ZX夏夜之风 <snwcreations@qq.com>
2022-07-14 10:22:22 +0800

    Add EventHandler#internal for API implementations.

--- src/main/java/snw/jkook/event/EventHandler.java
+++ src/main/java/snw/jkook/event/EventHandler.java
@@ -27,4 +27,11 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
+
+    /**
+     * Turn this to true to mark this handler as the internal event listener. <p>
+     * Internal event listeners will be executed before the normal listener executes. <p>
+     * Bot should <b>NEVER USE</b> this attribute, this is designed for API implementations.
+     */
+    boolean internal() default false;
 }
