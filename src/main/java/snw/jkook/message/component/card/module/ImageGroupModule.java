5d5a7bd0fa54c413a61ca6a3fd31b445d29acde2
ZX夏夜之风 <snwcreations@qq.com>
2022-07-17 16:31:01 +0800

    BREAKING CHANGE: ImageGroupModule no longer accepts FileComponent.

--- src/main/java/snw/jkook/message/component/card/module/ImageGroupModule.java
+++ src/main/java/snw/jkook/message/component/card/module/ImageGroupModule.java
@@ -17,7 +17,6 @@
 package snw.jkook.message.component.card.module;
 
 import org.apache.commons.lang.Validate;
-import snw.jkook.message.component.FileComponent;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -27,19 +26,18 @@ import java.util.List;
  * Represents a group of image components.
  */
 public class ImageGroupModule extends BaseModule {
-    private final List<FileComponent> images;
+    private final List<ImageModule> images;
 
-    public ImageGroupModule(List<FileComponent> images) {
+    public ImageGroupModule(List<ImageModule> images) {
         Validate.isTrue(images.size() >= 1, "Unexpected image module count, expected >= 1, got " + images.size());
         Validate.isTrue(images.size() <= 9, "Unexpected image module count, expected <= 9, got " + images.size());
-        Validate.isTrue(images.stream().allMatch(IT -> IT.getType() == FileComponent.Type.IMAGE), "The container only contains the image files.");
         this.images = Collections.unmodifiableList(new LinkedList<>(images));
     }
 
     /**
      * Get the images that already stored in this module.
      */
-    public List<FileComponent> getImages() {
+    public List<ImageModule> getImages() {
         return images;
     }
 }
