dfc0214e5b701ee7c7f035a390781e7f77bc2cfe
ZX夏夜之风 <snwcreations@qq.com>
2022-07-17 16:28:58 +0800

    BREAKING CHANGE: ContainerModule no longer accepts FileComponent.

--- src/main/java/snw/jkook/message/component/card/module/ContainerModule.java
+++ src/main/java/snw/jkook/message/component/card/module/ContainerModule.java
@@ -17,7 +17,6 @@
 package snw.jkook.message.component.card.module;
 
 import org.apache.commons.lang.Validate;
-import snw.jkook.message.component.FileComponent;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -28,19 +27,18 @@ import java.util.List;
  * But it is different from {@link ImageGroupModule}, the image is not cropped to a square. Multiple images are arranged vertically.
  */
 public class ContainerModule extends BaseModule {
-    private final List<FileComponent> images;
+    private final List<ImageModule> images;
 
-    public ContainerModule(List<FileComponent> images) {
+    public ContainerModule(List<ImageModule> images) {
         Validate.isTrue(images.size() >= 1, "Unexpected image module count, expected >= 1, got " + images.size());
         Validate.isTrue(images.size() <= 9, "Unexpected image module count, expected <= 9, got " + images.size());
-        Validate.isTrue(images.stream().allMatch(IT -> IT.getType() == FileComponent.Type.IMAGE), "The container only contains the image files.");
         this.images = Collections.unmodifiableList(new LinkedList<>(images));
     }
 
     /**
      * Get the images that already stored in this module.
      */
-    public List<FileComponent> getImages() {
+    public List<ImageModule> getImages() {
         return images;
     }
 }
