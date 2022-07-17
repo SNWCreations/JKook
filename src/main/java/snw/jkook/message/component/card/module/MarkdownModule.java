4e9be54e161e565bc82c7d6a49458a5f394d1b67
ZX夏夜之风 <snwcreations@qq.com>
2022-07-17 14:31:24 +0800

    BREAKING CHANGE: Now MarkdownModule constructor accepts Accessory.

--- src/main/java/snw/jkook/message/component/card/module/MarkdownModule.java
+++ src/main/java/snw/jkook/message/component/card/module/MarkdownModule.java
@@ -17,17 +17,20 @@
 package snw.jkook.message.component.card.module;
 
 import org.apache.commons.lang.Validate;
+import snw.jkook.entity.abilities.Accessory;
 
 /**
  * Represents the module with Markdown text.
  */
 public class MarkdownModule extends BaseModule {
     private final String value;
+    private final Accessory accessory;
 
-    public MarkdownModule(String value) {
+    public MarkdownModule(String value, Accessory accessory) {
         Validate.notNull(value, "The value cannot be null");
         Validate.isTrue(value.length() <= 5000, "Unexpected content length, expected <= 5000, got " + value.length());
         this.value = value;
+        this.accessory = accessory;
     }
 
     /**
@@ -35,5 +38,12 @@ public class MarkdownModule extends BaseModule {
      */
     public String getValue() {
         return value;
+    }
+
+    /**
+     * Get the accessory that bound to this module.
+     */
+    public Accessory getAccessory() {
+        return accessory;
     }
 }
