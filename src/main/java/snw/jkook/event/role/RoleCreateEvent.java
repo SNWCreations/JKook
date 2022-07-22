28d46ed734779213c02ad8a1b237a41095b742d1
ZX夏夜之风 <snwcreations@qq.com>
2022-07-22 11:05:08 +0800

    BREAKING CHANGE: Remove operator argument for Role(Create/Delete)Event.

--- src/main/java/snw/jkook/event/role/RoleCreateEvent.java
+++ src/main/java/snw/jkook/event/role/RoleCreateEvent.java
@@ -17,10 +17,7 @@
 package snw.jkook.event.role;
 
 import snw.jkook.entity.Role;
-import snw.jkook.entity.User;
 import snw.jkook.event.HandlerList;
-
-import java.util.Objects;
 
 /**
  * Represents an event related to a role was created.
@@ -28,18 +25,8 @@ import java.util.Objects;
 public class RoleCreateEvent extends RoleEvent {
     private static final HandlerList handlers = new HandlerList();
 
-    private final User operator;
-
-    public RoleCreateEvent(final long timeStamp, final Role role, final User operator) {
+    public RoleCreateEvent(final long timeStamp, final Role role) {
         super(timeStamp, role);
-        this.operator = Objects.requireNonNull(operator);
-    }
-
-    /**
-     * Get the operator of the new role.
-     */
-    public User getOperator() {
-        return operator;
     }
 
     public static HandlerList getHandlers() {
--- src/main/java/snw/jkook/event/role/RoleDeleteEvent.java
+++ src/main/java/snw/jkook/event/role/RoleDeleteEvent.java
@@ -17,10 +17,7 @@
 package snw.jkook.event.role;
 
 import snw.jkook.entity.Role;
-import snw.jkook.entity.User;
 import snw.jkook.event.HandlerList;
-
-import java.util.Objects;
 
 /**
  * Represents an event related to a role was deleted.
@@ -28,18 +25,8 @@ import java.util.Objects;
 public class RoleDeleteEvent extends RoleEvent {
     private static final HandlerList handlers = new HandlerList();
 
-    private final User operator;
-
-    public RoleDeleteEvent(final long timeStamp, final Role role, final User operator) {
+    public RoleDeleteEvent(final long timeStamp, final Role role) {
         super(timeStamp, role);
-        this.operator = Objects.requireNonNull(operator);
-    }
-
-    /**
-     * Get the operator.
-     */
-    public User getOperator() {
-        return operator;
     }
 
     public static HandlerList getHandlers() {
