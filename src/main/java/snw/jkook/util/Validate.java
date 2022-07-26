f9461ea2a685e71bf4e4765e76b0c251179aa62c
ZX夏夜之风 <snwcreations@qq.com>
2022-07-26 11:22:07 +0800

    Add Validate class that written by myself.
    I will use this instead of Apache Commons Lang.

---
+++ src/main/java/snw/jkook/util/Validate.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 JKook contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package snw.jkook.util;
+
+/**
+ * A small class for validating.
+ */
+public class Validate {
+
+    private Validate() {}
+
+    public static void isTrue(boolean expression, String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void isTrue(boolean expression) {
+        if (!expression) {
+            throw new IllegalArgumentException("The validated expression is false");
+        }
+    }
+
+    public static void isFalse(boolean expression) {
+        isFalse(expression, "The validated expression is true");
+    }
+
+    public static void isFalse(boolean expression, String message) {
+        isTrue(!expression, message);
+    }
+
+    public static void notNull(Object object) {
+        notNull(object, "The validated object is null");
+    }
+
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void notEmpty(String string, String message) {
+        if (string == null || string.length() == 0) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void notEmpty(String string) {
+        notEmpty(string, "The validated string is empty");
+    }
+
+}
