cd77bfb7744376513ae2f53c7eb54e26fb8660c2
ZX夏夜之风 <snwcreations@qq.com>
2022-08-03 16:18:36 +0800

    Bring back the ContextModule. (I deleted it in commit d1ea04727fde4d59bba5462df05f6d5fc2b9ae01 by mistake)

---
+++ src/main/java/snw/jkook/message/component/card/module/ContextModule.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 JKook contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package snw.jkook.message.component.card.module;
+
+import snw.jkook.message.component.card.element.BaseElement;
+import snw.jkook.message.component.card.element.ImageElement;
+import snw.jkook.message.component.card.element.PlainTextElement;
+import snw.jkook.util.Validate;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ContextModule extends BaseModule {
+    private final List<BaseElement> modules;
+
+    public ContextModule(List<BaseElement> modules) {
+        Validate.isTrue(modules.stream().allMatch(IT -> ((IT instanceof PlainTextElement) || (IT instanceof ImageElement))), "Context module only accepts plain-text, kmarkdown or image modules.");
+        this.modules = Collections.unmodifiableList(modules);
+    }
+
+    /**
+     * Get the modules that stored in this module.
+     */
+    public List<BaseElement> getModules() {
+        return modules;
+    }
+
+    /**
+     * A simple builder for building {@link ContextModule}.
+     */
+    public static class Builder {
+        private final List<BaseElement> modules = new LinkedList<>();
+
+        public Builder add(PlainTextElement module) {
+            modules.add(module);
+            return this;
+        }
+
+        public Builder add(ImageElement module) {
+            modules.add(module);
+            return this;
+        }
+
+        public ContextModule build() {
+            return new ContextModule(modules);
+        }
+    }
+}
