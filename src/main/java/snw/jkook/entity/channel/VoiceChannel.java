ddad1818aa5078a24af3735aede0d1d60cbde2ca
ZX夏夜之风 <snwcreations@qq.com>
2022-08-06 15:24:53 +0800

    -1 not represents infinity, but 0 does it.

--- src/main/java/snw/jkook/entity/channel/VoiceChannel.java
+++ src/main/java/snw/jkook/entity/channel/VoiceChannel.java
@@ -29,9 +29,9 @@ public interface VoiceChannel extends Channel {
 
     /**
      * Return the max user counts that this channel can hold. <p>
-     * If this channel can hold infinity users, <code>-1</code> will be returned.
+     * If this channel can hold infinity users, <code>0</code> will be returned.
      */
-    int getMaxSize(); // return -1 if this channel's size is infinity.
+    int getMaxSize();
 
     /**
      * Get the users that already joined this channel.
