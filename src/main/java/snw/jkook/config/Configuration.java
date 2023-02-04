8187e708519cae2058e0aaf7bd2bb6437a8f51e4
ZX夏夜之风 <snwcreations@qq.com>
2023-02-04 12:05:47 +0800

    Translated a little things in configuration package...

--- src/main/java/snw/jkook/config/Configuration.java
+++ src/main/java/snw/jkook/config/Configuration.java
@@ -22,83 +22,68 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 
 /**
- * Represents a source of configurable options and settings
+ * 配置文件与相关设置的基类。
  */
 public interface Configuration extends ConfigurationSection {
+
     /**
-     * Sets the default value of the given path as provided.
-     * <p>
-     * If no source {@link Configuration} was provided as a default
-     * collection, then a new {@link MemoryConfiguration} will be created to
-     * hold the new default value.
-     * <p>
-     * If value is null, the value will be removed from the default
-     * Configuration source.
+     * 设置指定路径的缺省值。<br>
+     * 如果没有默认的 {@link Configuration} 在对应的路径上，
+     * 那么将会建立一个新的 {@link MemoryConfiguration} 用于保存。<br>
+     * 如果值为 {@code null} ，该值将被从默认的配置源中删除。
      *
-     * @param path  Path of the value to set.
-     * @param value Value to set the default to.
-     * @throws IllegalArgumentException Thrown if path is null.
+     * @param path 路径
+     * @param value 缺省值
+     * @throws IllegalArgumentException 路径为 {@code null} 时抛出此异常
      */
     @Override
     void addDefault(@NotNull String path, @Nullable Object value);
 
     /**
-     * Sets the default values of the given paths as provided.
+     * 将指定 {@link Map} 中的键值都加入到此配置对象的缺省值。<br>
+     * 如果没有缺省值 {@link Configuration} 用于存储，那么将会建立一个新的 {@link MemoryConfiguration} 用于保存缺省值。
      * <p>
-     * If no source {@link Configuration} was provided as a default
-     * collection, then a new {@link MemoryConfiguration} will be created to
-     * hold the new default values.
+     * 如果值为 {@code null} ，将会删除该路径上的缺省值.
      *
-     * @param defaults A map of Path{@literal ->}Values to add to defaults.
-     * @throws IllegalArgumentException Thrown if defaults is null.
+     * @param defaults 键为路径，值为对应路径的值
+     * @throws IllegalArgumentException {@code defaults} 为 {@code null} 时抛出此异常
      */
     void addDefaults(@NotNull Map<String, Object> defaults);
 
     /**
-     * Sets the default values of the given paths as provided.
-     * <p>
-     * If no source {@link Configuration} was provided as a default
-     * collection, then a new {@link MemoryConfiguration} will be created to
-     * hold the new default value.
-     * <p>
-     * This method will not hold a reference to the specified Configuration,
-     * nor will it automatically update if that Configuration ever changes. If
-     * you require this, you should set the default source with {@link
-     * #setDefaults(snw.jkook.config.Configuration)}.
+     * 把指定 {@link Configuration} 中的所有数据全部加入到缺省值对象。<br>
+     * 如果没有缺省值 {@link Configuration}, 那么将会建立一个新的 {@link snw.jkook.config.MemoryConfiguration} 用于保存。<br>
+     * 你可以使用 {@link #setDefaults(snw.jkook.config.Configuration)} 来设置缺省值对象来源。
      *
-     * @param defaults A configuration holding a list of defaults to copy.
-     * @throws IllegalArgumentException Thrown if defaults is null or this.
+     * @param defaults 数据来源
+     * @throws IllegalArgumentException 指定的 {@code defaults} 为 {@code null} 或者自己时抛出
      */
     void addDefaults(@NotNull Configuration defaults);
 
     /**
-     * Sets the source of all default values for this {@link Configuration}.
-     * <p>
-     * If a previous source was set, or previous default values were defined,
-     * then they will not be copied to the new source.
+     * 设置新的缺省值对象。<br>
+     * 若此配置对象已有缺省值对象，则将直接替换原有的。
      *
-     * @param defaults New source of default values for this configuration.
-     * @throws IllegalArgumentException Thrown if defaults is null or this.
+     * @param defaults 将作为新的缺省值对象的 {@link Configuration} 对象
+     * @throws IllegalArgumentException 指定的 {@code defaults} 为 {@code null} 或者是本身原有的缺省值对象时抛出
      */
     void setDefaults(@NotNull Configuration defaults);
 
     /**
-     * Gets the source {@link Configuration} for this configuration.
-     * <p>
-     * If no configuration source was set, but default values were added, then
-     * a {@link MemoryConfiguration} will be returned. If no source was set
-     * and no defaults were set, then this method will return null.
+     * 获取这个 {@link Configuration} 的缺省值 {@link Configuration} 对象。<br>
+     * 如果设置过缺省值，即使没有设置缺省值对象，也会返回 {@link Configuration} 。(原因见 {@link #addDefault(String, Object)})<br>
+     * 如果都没有，则返回 null 。
      *
-     * @return Configuration source for default values, or null if none exist.
+     * @return 缺省值对象，如果没有则返回 {@code null}
      */
     @Nullable Configuration getDefaults();
 
     /**
-     * Gets the {@link ConfigurationOptions} for this {@link Configuration}.
-     * <p>
-     * All setters through this method are chainable.
+     * 获取这个 {@link Configuration} 的 {@link ConfigurationOptions} 设置对象。<br>
+     * 如果需要修改配置对象，直接修改返回值就可以了。
      *
-     * @return Options for this configuration
+     * @return 这个配置文件的一些配置
+     * @see ConfigurationOptions
      */
     @NotNull ConfigurationOptions options();
 }
--- src/main/java/snw/jkook/config/ConfigurationOptions.java
+++ src/main/java/snw/jkook/config/ConfigurationOptions.java
@@ -19,8 +19,8 @@ package snw.jkook.config;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Various settings for controlling the input and output of a {@link
- * Configuration}
+ * {@link Configuration} 的设置类。<br>
+ * 它有一部分基于建造者模式设计。
  */
 public class ConfigurationOptions {
     private char pathSeparator = '.';
@@ -32,9 +32,9 @@ public class ConfigurationOptions {
     }
 
     /**
-     * Returns the {@link Configuration} that this object is responsible for.
+     * 返回关联这个配置的 {@link Configuration} 。
      *
-     * @return Parent configuration
+     * @return 关联的配置对象
      */
     @NotNull
     public Configuration configuration() {
@@ -42,27 +42,25 @@ public class ConfigurationOptions {
     }
 
     /**
-     * Gets the char that will be used to separate {@link
-     * ConfigurationSection}s
-     * <p>
-     * This value does not affect how the {@link Configuration} is stored,
-     * only in how you access the data. The default value is '.'.
+     * 获取用于分离 {@link ConfigurationSection} 中路径的 char 对象。<br>
+     * 这并不会影响数据的储存，它只是路径的分隔符。<br>
+     * 这只会影响你在程序中怎样读取数据。<br>
+     * 举个例子:<br>
+     * 你有一个配置对象，但是它的 {@code pathSeparator} 是 "," ，那么像 "snw.name" 这种键就不是指 "snw" 项下的 "name" 项了，
+     * 这时获取原 "snw.name" 的数据就要用 "snw,name" 作为键。<br>
+     * 这与 Microsoft Windows 使用 "\" 作为路径分隔符，而 GNU/Linux 使用 "/" 是一个道理。
      *
-     * @return Path separator
+     * @return 路径分割符
      */
     public char pathSeparator() {
         return pathSeparator;
     }
 
     /**
-     * Sets the char that will be used to separate {@link
-     * ConfigurationSection}s
-     * <p>
-     * This value does not affect how the {@link Configuration} is stored,
-     * only in how you access the data. The default value is '.'.
+     * 设置用于分离 {@link ConfigurationSection} 中路径的 char 对象。<br>
+     * 关于这个 {@code char} 对象的解释见 {@link #pathSeparator()} 方法。
      *
-     * @param value Path separator
-     * @return This object, for chaining
+     * @param value 路径分割符
      */
     @NotNull
     public ConfigurationOptions pathSeparator(char value) {
@@ -71,37 +69,22 @@ public class ConfigurationOptions {
     }
 
     /**
-     * Checks if the {@link Configuration} should copy values from its default
-     * {@link Configuration} directly.
-     * <p>
-     * If this is true, all values in the default Configuration will be
-     * directly copied, making it impossible to distinguish between values
-     * that were set and values that are provided by default. As a result,
-     * {@link ConfigurationSection#contains(java.lang.String)} will always
-     * return the same value as {@link
-     * ConfigurationSection#isSet(java.lang.String)}. The default value is
-     * false.
+     * 检查与这个设置对象关联的 {@link Configuration} 的值是否直接从缺省值 {@link Configuration} 复制。<br>
+     * 如果为 {@code true} ，则关联的 {@link Configuration} 中的值都是从缺省值 {@link Configuration} 中复制的。<br>
+     * 在这时，这个列表将被锁定. 并始终返回缺省值对象中的值. 可以看作是只读的缺省值对象。<br>
+     * 默认为 {@code false} 。
      *
-     * @return Whether or not defaults are directly copied
+     * @return 与这个设置对象关联的 {@link Configuration} 的值是否直接从缺省值 {@link Configuration} 复制
      */
     public boolean copyDefaults() {
         return copyDefaults;
     }
 
     /**
-     * Sets if the {@link Configuration} should copy values from its default
-     * {@link Configuration} directly.
-     * <p>
-     * If this is true, all values in the default Configuration will be
-     * directly copied, making it impossible to distinguish between values
-     * that were set and values that are provided by default. As a result,
-     * {@link ConfigurationSection#contains(java.lang.String)} will always
-     * return the same value as {@link
-     * ConfigurationSection#isSet(java.lang.String)}. The default value is
-     * false.
+     * 设置与这个设置对象关联的 {@link Configuration} 的值是否直接从缺省值 {@link Configuration} 复制。
      *
-     * @param value Whether or not defaults are directly copied
-     * @return This object, for chaining
+     * @param value 与这个设置对象关联的 {@link Configuration} 的值是否直接从缺省值 {@link Configuration} 复制
+     * @see #copyDefaults()
      */
     @NotNull
     public ConfigurationOptions copyDefaults(boolean value) {
