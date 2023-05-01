f4aaa7e357589c865f1182837faa2bba33ec826b
ZX夏夜之风 <snwcreations@qq.com>
2023-05-01 10:43:26 +0800

    Fix incorrect import in PluginManager
    Caused by #17

--- src/main/java/snw/jkook/plugin/PluginManager.java
+++ src/main/java/snw/jkook/plugin/PluginManager.java
@@ -20,7 +20,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.util.function.Preidicate;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**

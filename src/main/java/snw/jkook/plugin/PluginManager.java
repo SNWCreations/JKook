SNWCreations/JKook

commit b197ef8ed1f3a86c6fd0d46eb6b86c18bd4e0ab1

Add method for registering plugin loader (#17)
Added a method named registerPluginLoader to PluginManager class so that people can register plugin loader to load the plugins which are not recognised by default plugin loader.

src/main/java/snw/jkook/plugin/PluginManager.java
@@ -20,6 +20,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.function.Preidicate;
+import java.util.function.Supplier;
 
 /**
  * Represents the plugin manager. Handles the management of the plugins.
@@ -124,4 +126,12 @@ public interface PluginManager {
      * @param plugin The plugin reference
      */
     void removePlugin(Plugin plugin);
+    
+    /**
+     * Register plugin loader for loading plugins which are not recognised by default plugin loader
+     * 
+     * @param condition Condition to check whether the file is loadable by the provided loader type
+     * @param provider PluginLoader provider
+     */
+    void registerPluginLoader(Predicate<File> condition, Supplier<PluginLoader> provider);
 }

commit f4aaa7e357589c865f1182837faa2bba33ec826b

Fix incorrect import in PluginManager
Caused by #17

src/main/java/snw/jkook/plugin/PluginManager.java
@@ -20,7 +20,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.util.function.Preidicate;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**