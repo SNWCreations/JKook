aff163388dc5a46193c96eb15da1d1c945526dce
huanmeng_qwq <1871735932@qq.com>
2024-09-01 14:59:42 +0800

    Migrate build toolchain to Gradle (#34)
    Sync with SNWCreations/KookBC#106

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    api("org.slf4j:slf4j-api:1.7.36")
+    api("org.yaml:snakeyaml:2.0")
+    compileOnly("org.jetbrains:annotations:23.0.0")
+}
+
+group = "snw"
+version = "0.53.0"
+description = "JKook"
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
+
+tasks.javadoc {
+    options.encoding = "UTF-8"
+}
